1ad11b8e1ccd309b062334ea19f5c6a1923ad42e
Paulstar200 <vitalispaul48@live.com>
2020-08-12 14:55:44 +0300

    Delete node and calculate length

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -46,11 +46,97 @@ class LinkedList:
         new_node.next = prev_node.next
         prev_node.next = new_node
 
+    
+    '''
+    Delete node
+    
+    Given a key (data field) delete node with this field
+    Assume elements in linked list are unique
+
+    Example: Delete node with data field "B"
+
+    Two cases:
+    - Node to be deleted is head
+    - Node to be deleted is not head
+    '''
+
+    def delete_node(self, key):
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    '''
+    Delete node at position
+    Given a position, delete node with this position.
+
+    Assume elements in linked list are unique
+
+    Example: Delete node with position 1
+    '''
+
+    def delete_node_at_pos(self, pos):
+        cur_node = self.head
+
+        if pos == 0:
+            self.head = cur_node.next
+            cur_node = None
+            return
+        
+        prev = None
+        count = 0
+        while cur_node and count != pos:
+            prev = cur_node
+            cur_node = cur_node.next
+            count += 1
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    '''
+    Calculating length of list
+    '''
+    def len_iterative(self):
+        count =  0
+        cur_node = self.head
+
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
+
+    def len_recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self.len_recursive(node.next)
 
 llist = LinkedList()
 llist.append('A')
 llist.append('B')
+llist.append('C')
+llist.append('D')
 # llist.prepend('C')
 
-llist.insert_after_node(llist.head, 'D')
-llist.print_list()
+# llist.insert_after_node(llist.head, 'D')
+
+# llist.delete_node_at_pos(1)
+
+# print(llist.len_iterative())
+print(llist.len_recursive(llist.head))
+# llist.print_list()
