930766eee50bf675d45c0e2e61afc2a605223a31
Paulstar200 <vitalispaul48@live.com>
2020-09-01 05:53:03 +0300

    Pre-order Traversal

--- BinaryTrees/binary_tree.py
+++ BinaryTrees/binary_tree.py
@@ -9,7 +9,25 @@ class BinaryTree:
     def __init__(self, root):
         self.root = Node(root)
 
+    # Responsible for printing out the tree and specifying which
+    # type of traversal algorithm we will want to print the tree out in
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False
 
+    def preorder_print(self, start, traversal):
+        """Root->Left->Right"""
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+# 1-2-4-5-3-6-7-
 #        1
 #      /  \
 #     2    3
@@ -22,3 +40,5 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
+
+print(tree.print_tree("preorder"))
