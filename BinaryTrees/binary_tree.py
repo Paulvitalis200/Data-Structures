e3a7433209f6a671ff1615043f45997c604287b5
Paulstar200 <vitalispaul48@live.com>
2020-09-20 12:02:38 +0300

    Level order traversal

--- BinaryTrees/binary_tree.py
+++ BinaryTrees/binary_tree.py
@@ -1,3 +1,27 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+    
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -16,6 +40,8 @@ class BinaryTree:
             return self.inorder_print(tree.root, "")
         elif traversal_type == "postorder":
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
         else:
             print("Traversal type "+ str(traversal_type) +  " is not supported.")
 
@@ -43,10 +69,30 @@ class BinaryTree:
             traversal += (str(start.value) + "-")
         return traversal
 
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
+
 
 #  1-2-4-5-3-6-7-
 # 4-2-5-1-6-3-7-
 # 4-2-5-6-3-7-1-
+# 1-2-3-4-5-6-7-
 #         1
 #     /       \
 #    2          3
@@ -63,4 +109,5 @@ tree.root.right.right = Node(7)
 
 # print(tree.print_tree("preorder"))
 # print(tree.print_tree("inorder"))
-print(tree.print_tree("postorder"))
+# print(tree.print_tree("postorder"))
+print(tree.print_tree("levelorder"))
