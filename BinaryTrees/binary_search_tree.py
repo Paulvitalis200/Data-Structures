3e378632643248188555c5714d32f01b3092087b
Paulstar200 <vitalispaul48@live.com>
2020-10-03 11:18:58 +0300

    Check the BST property

--- BinaryTrees/binary_search_tree.py
+++ BinaryTrees/binary_search_tree.py
@@ -47,6 +47,38 @@ class BST:
         if data == cur_node.data:
             return True
 
+    def in_order_print_tree(self):
+        if self.root:
+            self._inorder_print_tree(self.root)
+
+    def _inorder_print_tree(self, cur_node):
+        if cur_node:
+            self._inorder_print_tree(cur_node.left)
+            print(str(cur_node.data))
+            self._inorder_print_tree(cur_node.right)
+
+    def is_bst_satisfied(self):
+        if self.root:
+            is_satisfied = self._is_bst_satisfied(self.root, self.root.data)
+
+            if is_satisfied is None:
+                return True
+            return False
+        return True
+
+    def _is_bst_satisfied(self, cur_node, data):
+        if cur_node.left:
+            if data > cur_node.left.data:
+                return self._is_bst_satisfied(cur_node.left, cur_node.left.data)
+            else:
+                return False
+        if cur_node.right:
+            if data < cur_node.right.data:
+                return self._is_bst_satisfied(cur_node.right, cur_node.right.data)
+            else:
+                return False
+
+
 bst = BST()
 
 bst.insert(4)
@@ -55,6 +87,13 @@ bst.insert(6)
 bst.insert(5)
 bst.insert(10)
 
-print(bst.find(4))
-print(bst.find(3))
-print(bst.find(10))
+tree = BST()
+tree.root = Node(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+
+# print(bst.in_order_print_tree())
+# print(tree.in_order_print_tree())
+
+print(bst.is_bst_satisfied())
+print(tree.is_bst_satisfied())
