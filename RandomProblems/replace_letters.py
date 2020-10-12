b3424f60a27fbc7a46a80cf2ba53b6aa49505e9c
Paulstar200 <vitalispaul48@live.com>
2020-10-13 02:23:53 +0300

    Replace every letter with its position in the alphabet

--- RandomProblems/replace_letters.py
+++ RandomProblems/replace_letters.py
@@ -11,9 +11,31 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 """
 
+# Time complexity: O(n)^2
+
+"""
+Methodology
+1. Define string of alphabets and an empty hashtable.
+2. Loop through the alphabets and place them in the hashtable with their
+corresponding number on the alphabet.
+3. Convert text to lowercase.
+4. For each letter, if its in the hash table, push it onto a new string.
+5. Return the new string.
+"""
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# def alphabet_position(text):
+#     if type(text) == str:
+#         text = text.lower()
+#         result = ''
+#         for letter in text:
+#             if letter.isalpha() == True:
+#                 result = result + ' ' + str(alphabet.index(letter) + 1)
+#         return result.lstrip(' ')
+
 def alphabet_position(text):
     alphabets = 'abcdefghijklmnopqrstuvwxyz'
-    alphabets = alphabets.lower()
     ht = dict()
     i = 0
     while i < len(alphabets):
