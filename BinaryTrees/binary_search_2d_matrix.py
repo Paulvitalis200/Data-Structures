2da91d6e31ba9a747f943f1d860a0e02ac48ba47
Paul Otieno <vitalispaul48@live.com>
2023-07-26 06:51:46 +0300

    Search 2D matrix

---
+++ BinaryTrees/binary_search_2d_matrix.py
@@ -0,0 +1,41 @@
+
+def searchMatrix(matrix, target: int) -> bool:
+    # Get dimensions. No of ROWS and COLS
+    ROWS, COLS = len(matrix), len(matrix[0])
+
+    # We now do our first binary search
+    # Now look for the row we need to find
+    # top - Top row
+    # bot - Bottom row
+    top, bot = 0, ROWS - 1
+
+    while top <= bot:
+        row = (top + bot) // 2
+
+        # Compare target value with the right most value in the row
+        if target > matrix[row][-1]:
+            top = row + 1
+        elif target < matrix[row][0]:
+            bot = row - 1
+        else:
+            break
+
+    # If none of the rows contain the target value
+    if not (top <= bot):
+        return False
+    row = (top + bot) //2
+    l, r = 0, COLS - 1
+    while l <= r:
+        m = (l + r) //2
+        if target > matrix[row][m]:
+            l = m + 1
+        elif target < matrix[row][m]:
+            r = m - 1
+        else:
+            return True
+
+    return False
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+searchMatrix(matrix, 3)
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-def high_and_low(numbers):
-    numbers_list = numbers.split(" ")
-    for i in range(len(numbers_list)):
-        numbers_list[i] = int(numbers_list[i])
-    output_str = str(max(numbers_list))  + " " + str(min(numbers_list))
-    print(numbers_list)
-    return output_str
+# def high_and_low(numbers):
+#     numbers_list = numbers.split(" ")
+#     for i in range(len(numbers_list)):
+#         numbers_list[i] = int(numbers_list[i])
+#     output_str = str(max(numbers_list))  + " " + str(min(numbers_list))
+#     print(numbers_list)
+#     return output_str
 
-print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+# print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
 
+
+def digital_root(n):
+    # ...
+    n = str(n)
+    
+    check_sum = call_sum(n)
+    if len(str(check_sum)) > 1:
+        return digital_root(check_sum)
+    else:
+        return check_sum
+    
+def call_sum(input_str):
+    x = [int(i) for i in input_str]
+    return sum(x)
+
+print(digital_root(942))
